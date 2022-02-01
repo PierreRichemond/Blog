e321b985f12109ab29358ba71bbf299169ad4863
= <pierreambroise@hotmail.fr>
2022-02-01 13:23:43 +0900

    add validation to article + tests

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -14,4 +14,19 @@ RSpec.feature 'Creating Articles' do
     expect(page).to have_content("Article has been created")
     expect(page.current_path).to eq(articles_path)
   end
+
+  scenario "A user fails to create a new article" do
+    visit "/" # capybara helper, makes sure we create the new article from the root page
+
+    click_link "New Article" # capybara helper, check the link name is New Article
+
+    fill_in "Title", with: "" # capybara helper,
+    fill_in "Body", with: "" # capybara helper,
+
+    click_button "Create Article"# capybara helper,
+
+    expect(page).to have_content("Article has not been created")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+  end
 end
