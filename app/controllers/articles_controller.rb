78a0ae4771fee38df88bbc8bb30d70a043ce3569
= <pierreambroise@hotmail.fr>
2022-02-01 14:55:55 +0900

    handle exception for article not found

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,8 +10,8 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-    flash[:success] = 'Article has been created'
-    redirect_to articles_path
+      flash[:success] = 'Article has been created'
+      redirect_to articles_path
     else
       flash.now[:danger] = 'Article has not been created'
       render :new
@@ -20,6 +20,15 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+
+  protected
+
+  def resource_not_found
+    message = "The article you are looking for could not be found."
+    flash[:alert] = message
+    redirect_to root_path
   end
 
   private
