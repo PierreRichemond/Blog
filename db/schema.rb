61812cf284626f057cee79e92882093f0cb615eb
= <pierreambroise@hotmail.fr>
2022-02-01 16:46:15 +0900

    changed DB from sqlite to postgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2022_02_01_023200) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
