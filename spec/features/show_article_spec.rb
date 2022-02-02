f3f10a51d138fcfc94cf1fe082f795f608f9f72f
= <pierreambroise@hotmail.fr>
2022-02-02 14:45:00 +0900

    update spec to verify only owner sees edit and delete buttons on articles

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -4,11 +4,11 @@ RSpec.feature 'Showing an article' do
 
   before do
     @john = User.create!(email: "john@example.com", password: "password")
-    login_as(@john)
+    @fred = User.create!(email: "fred@example.com", password: "password")
     @article = Article.create(title: "First Article", body: 'Lorem Ipsum', user: @john)
   end
 
-  scenario 'A user show an article' do
+  scenario 'non-signed in users, hide edit and delete buttons' do
     visit '/'
 
     click_link @article.title
@@ -16,5 +16,36 @@ RSpec.feature 'Showing an article' do
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
     expect(current_path).to eq(article_path(@article))
+
+    expect(page).not_to have_link('Edit Article')
+    expect(page).not_to have_link('Delete Article')
+  end
+
+  scenario 'non-owner in users, hide edit and delete buttons as well' do
+    login_as(@fred)
+    visit '/'
+
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+    expect(page).not_to have_link('Edit Article')
+    expect(page).not_to have_link('Delete Article')
+  end
+
+  scenario 'A signed in user sees both Edit and Delete buttons on his articles' do
+    login_as(@john)
+    visit '/'
+
+    click_link @article.title
+
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+    expect(page).to have_link('Edit Article')
+    expect(page).to have_link('Delete Article')
   end
 end
