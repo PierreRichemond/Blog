de57d42dd7e0279b5a759116a981c27cf671e558
= <pierreambroise@hotmail.fr>
2022-02-02 10:12:04 +0900

    test to push to heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 //= require jquery
-
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
-import "bootstrap";
-import "../styles/application.scss"
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require_tree .
