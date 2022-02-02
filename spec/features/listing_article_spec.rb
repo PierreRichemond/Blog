2de1bcc2fd0af5d55bca90c7e8e281a69a313f0d
= <pierreambroise@hotmail.fr>
2022-02-02 14:14:51 +0900

    added button to create article only for signed in users

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.feature 'Listing Articles' do
   before do
     @john = User.create!(email: "john@example.com", password: "password")
-    login_as(@john)
     @article1 = Article.create(title: "First Article", body: 'Lorem Ipsum', user: @john)
     @article2 = Article.create(title: "Second Article", body: 'Body of the second article', user: @john)
   end
@@ -17,6 +16,32 @@ RSpec.feature 'Listing Articles' do
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+  end
+
+  scenario 'With article created but user not signed in' do
+    visit '/'
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link('New Article')
+  end
+
+  scenario 'With article created and user signed in' do
+    login_as(@john)
+    visit '/'
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).to have_link('New Article')
+    expect(@article1.user).to eq(@john)
   end
 
   scenario 'A user has no articles' do
